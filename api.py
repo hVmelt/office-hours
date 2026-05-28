a4a8492bfa3565ad48b5c1f7a3df24eede33f2f6
Sarwat <sarwat.saadaat@gmail.com>
2026-05-28 15:09:05 -0500

    Day 8: Allow live Vercel domain in CORS (2)

--- api.py
+++ api.py
@@ -36,7 +36,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://office-hours-two.vercel.app/"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://office-hours-two.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
